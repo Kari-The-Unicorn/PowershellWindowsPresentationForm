66b8f8d1f15dfd138f414751f0c87756e4f9dfdf
gh-unicorn <inmatrixos@gmail.com>
2020-01-12 16:24:37 +0000

    ps file with simple windows presentation form

---
+++ PowershellWPF.ps1
@@ -0,0 +1,12 @@
+﻿[xml]$xaml = @"
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    x:Name="Window" Title="My Window" WindowStartupLocation = "CenterScreen" 
+    Width = "800" Height = "600">
+</Window>
+"@ 
+
+$reader=(New-Object System.Xml.XmlNodeReader $xaml)
+$Window=[Windows.Markup.XamlReader]::Load( $reader )
+$Window.ShowDialog()
